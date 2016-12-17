1322b8b594e74bc70bac5f33e1e6fd19c00200f5
YoRcH <the.yorch@gmail.com>
2016-12-16 19:23:41 -0600

    uat test

--- classes/test.php
+++ classes/test.php
@@ -4,8 +4,8 @@ require "../config.php";
 
 $app =  new V3Site($admin, $password, $dir);
 
-if ($app->checkAvailability("yorchi"))
-	$app->makeApp("yorchi");
+if ($app->checkAvailability("uat"))
+	$app->makeApp("uat");
 else
 	echo "Application Name not Available\n";
 ?>
